c59366ef744560292351205f105817b8f52c4956
iamjon <murodjon.tursunpulatov@gmail.com>
2020-11-05 19:29:34 +0500

    'new'

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { useStateValue } from "./StateProvider";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { auth } from "./firebase";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders";
+
+const promise = loadStripe(
+  "pk_test_51HjesvLROzjIIwkh4gDJk6UXQ0RKjXbEgjXdGkrO091H9gwYXkZlYbQYa1KWKdxiGXFGmgZna1rBqsRcNwAjeazg00sRdGSbh3"
+);
+
 function App() {
   const [{}, dispatch] = useStateValue();
 
@@ -38,10 +47,20 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
 
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
